059fbf72f24878e33ebae48dfeb99d6ed8572370
frankgon1627 <fg1333762@gmail.com>
2025-06-24 21:06:01 -0400

    clean up

--- dummy_sims/control_affine_circle_traj_1st_order_cbf.py
+++ dummy_sims/control_affine_circle_traj_1st_order_cbf.py
@@ -136,7 +136,7 @@ for _ in range(steps):
     x += v * np.cos(theta) * dt + np.random.normal(0, sigma_pos)
     y += v * np.sin(theta) * dt + np.random.normal(0, sigma_pos)
     theta += omega * dt + np.random.normal(0, sigma_theta)
-    theta = (theta + np.pi) % (2 * np.pi) - np.pi  # Normalize
+    theta = (theta + np.pi) % (2 * np.pi) - np.pi 
 
     state = np.array([x, y, theta])
     trajectory.append(state[:2].copy())
@@ -164,7 +164,7 @@ ax.add_patch(circle)
 # Plot handles
 point, = ax.plot([], [], 'bo', label="Robot")
 trail, = ax.plot([], [], 'b-', lw=1, label="Path")
-ref_dot, = ax.plot([], [], 'gx', label="Ref Point")  # Green X
+ref_dot, = ax.plot([], [], 'gx', label="Ref Point") 
 
 def update(frame):
     point.set_data(trajectory[frame, 0], trajectory[frame, 1])
